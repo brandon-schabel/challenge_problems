4e21509674926a4443d9b1caaee4bddd6cf19eef
Brandon Schabel <brandonschabel1995@gmail.com>
2018-09-29 12:58:16 -0700

    two to one

---
+++ JavaScript/codewars/two_to_one.js
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/5656b6906de340bd1b0000ac/train/javascript
+constlongest = (s1, s2) => {
+  s1 = s1.split('')
+  s2 = s2.split('')
+  let returnArr = []
+  
+  s1.forEach(element =>{
+      if(returnArr.indexOf(element) === -1) {
+        returnArr.push(element)
+      }
+    }
+  )
+  
+  s2.forEach(element =>{
+      if(returnArr.indexOf(element) === -1) {
+        returnArr.push(element)
+      }
+    }
+  )
+  
+  return returnArr.sort().join('');
+}
+
