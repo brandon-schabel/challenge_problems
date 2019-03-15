5f43391abd09e81b2be1ff06af6d6c8239ee4cf2
brandon-schabel <brandonschabel1995@gmail.com>
2019-03-14 20:33:13 -0700

    ten min walk problem

---
+++ everyday_practice/14Mar2019/tenMinWalk.js
@@ -0,0 +1,26 @@
+function isValidWalk(walk) {
+  //insert brilliant code here
+  if(walk.length !== 10) return false
+  
+  let NS = 0;
+  let EW = 0;
+  
+  walk.forEach(direction => {
+    switch(direction) {
+      case 'n':
+        NS++;
+        break;
+      case 's':
+        NS--;
+        break;
+      case 'e':
+        EW++;
+        break;
+      case 'w':
+        EW--;
+        break;
+    }
+  })
+  
+  return NS + EW === 0 ? true : false
+}
