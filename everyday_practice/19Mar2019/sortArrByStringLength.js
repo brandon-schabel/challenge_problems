9256e6a6bf13da09f63d04668c2bc36254853569
brandon-schabel <brandonschabel1995@gmail.com>
2019-03-19 19:34:24 -0700

    sort arr by string length

---
+++ everyday_practice/19Mar2019/sortArrByStringLength.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c/solutions/javascript
+function sortByLength (array) {
+  // Return an array containing the same strings, ordered from shortest to longest
+  return array.sort((a,b) => a.length - b.length)
+};
