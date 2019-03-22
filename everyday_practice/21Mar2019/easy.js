af385ecf2c972c545b524581632728fa2b4ec816
brandon-schabel <brandonschabel1995@gmail.com>
2019-03-21 22:08:59 -0700

    create phone number, reverse str, isdivisible

--- everyday_practice/21Mar2019/easy.js
+++ everyday_practice/21Mar2019/easy.js
@@ -15,3 +15,16 @@ function litres(time) {
 function invert(array) {
   return array.map(n => n !== 0 ? n * -1 : 0);
 }
+
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0 ? true : false;
+}
+
+function reverseStr(str){
+  return str.split('').reverse().join('');
+}
+
+function createPhoneNumber(numbers){
+  let n = numbers;
+  return `(${n[0]}${n[1]}${n[2]}) ${n[3]}${n[4]}${n[5]}-${n[6]}${n[7]}${n[8]}${n[9]}`
+}
