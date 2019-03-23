5d1646154216089698aa11329e0a9c591964375e
brandon-schabel <brandonschabel1995@gmail.com>
2019-03-22 19:23:32 -0700

    check if beast can bring feast

--- everyday_practice/22Mar2019/index.js
+++ everyday_practice/22Mar2019/index.js
@@ -32,3 +32,12 @@ function bonusTime(salary, bonus) {
       return `£${salary}`
     }
   }
+
+  function feast(beast, dish) {
+    //your function here
+      if(beast[0] === dish[0] && beast[beast.length-1] === dish[dish.length-1]) {
+        return true
+      } else {
+        return false
+      }
+    }
