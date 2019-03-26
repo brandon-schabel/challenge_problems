7db611bd38e1e83fc5a25505f6b941ed18e1dc35
brandon-schabel <brandonschabel1995@gmail.com>
2019-03-26 15:47:08 -0700

    reverse linked list

--- everyday_practice/26Mar2019/linkedList/singlyLinkedList.js
+++ everyday_practice/26Mar2019/linkedList/singlyLinkedList.js
@@ -1,8 +1,6 @@
 // linked list has a head, lenght, and a tail
 // visualgo.net
 
-
-
 // var first = new Node("hi")
 // first.next = new Node("there")
 // first.next.next = new Node("how")
@@ -11,33 +9,33 @@
 
 class Node {
   constructor(val) {
-    this.val = val;
-    this.next = null;
+    this.val = val
+    this.next = null
   }
 }
 
-class SinglyLinkedList{
-  constructor(){
-    this.head = null;
-    this.tail = null;
+class SinglyLinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
     this.length = 0
   }
 
   push(val) {
     var newNode = new Node(val)
     //if a head does not exist, then head and tail get set to the newNode
-    if(!this.head) {
-      this.head = newNode;
+    if (!this.head) {
+      this.head = newNode
       this.tail = this.head
-    } else{
+    } else {
       // otherwise take the current tail and set it to the new node
-      this.tail.next = newNode;
+      this.tail.next = newNode
       // then move the tail market over to the new node
       this.tail = newNode
     }
 
-    this.length++;
-    return this; //returns the whole list
+    this.length++
+    return this //returns the whole list
   }
 
   // traverse() {
@@ -51,11 +49,11 @@ class SinglyLinkedList{
   // }
 
   pop() {
-    if(!this.head) return undefined
-    let current = this.head;
-    let newTail = current;
-    while(current) {
-      newTail = current;
+    if (!this.head) return undefined
+    let current = this.head
+    let newTail = current
+    while (current) {
+      newTail = current
       current = current.next
     }
 
@@ -65,44 +63,116 @@ class SinglyLinkedList{
     this.tail.next = null
 
     this.length--
-    if(this.length === 0) {
-      this.head = null;
-      this.tail = null;
+    if (this.length === 0) {
+      this.head = null
+      this.tail = null
     }
     return current
   }
 
   shift() {
-    if(!this.head) return undefined
+    if (!this.head) return undefined
     const currHead = this.head
-    this.head = this.head.next;
+    this.head = this.head.next
     this.length--
-    if(this.length === 0) {
-      this.tail = null;
+    if (this.length === 0) {
+      this.tail = null
     }
     return currHead
   }
 
   unshift(val) {
-    let newNode = new Node(val);
+    let newNode = new Node(val)
 
-    if(!this.head) {
-      this.head = newNode;
+    if (!this.head) {
+      this.head = newNode
       this.tail = this.head
     } else {
       newNode.next = this.head
       this.head = newNode
     }
-    
+
     this.length++
 
-    return this;
+    return this
+  }
+
+  get(index) {
+    if (index < 0 || index > this.length) return null
+    let counter = 0
+
+    let current = this.head
+
+    while (counter !== index) {
+      current = current.next
+      counter++
+    }
+    return current
+  }
+
+  set(index, val) {
+    let selectedNode = this.get(index)
+    console.log(selectedNode)
+    if (selectedNode) {
+      selectedNode.val = val
+      return true
+    }
+    return false
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return null
+    if (index === 0) !!this.unshift(val) //return true if it worked, otherwise returns false
+    if (index === this.length) !!this.push(val)
+
+    let newNode = new Node(val)
+    let prevNode = this.get(index - 1)
+    let temp = prevNode.next
+    prevNode.next = newNode
+    newNode.next = temp
+    this.length++
+    return true
+  }
+
+  remove(index) {
+    if (index < 0 || index > this.length) return null
+    if(index === 0) return this.shift();
+    if(index === this.length - 1) return this.pop();
+    let previousNode = this.get(index-1)
+    var removed = previousNode.next
+    previousNode.next = removed.next
+    this.length--
+    return removed
+  }
+
+  // https://www.udemy.com/js-algorithms-and-data-structures-masterclass/learn/lecture/11078964#overview
+  reverse(){
+    //temp store our head
+    var node = this.head
+    // set our head to our tail
+    this.head = this.tail
+    // set the tail to our head
+    this.tail = node
+
+    let next;
+    // we set prev to null becasuse we want the tail to be null
+    var prev = null;
+
+    for(let i = 0; i < this.length; i++) {
+      // next will be the next of whatever we're currently looking at
+      next = node.next
+      node.next = prev;
+
+      prev = node;
+      node = next
+    }
+
+    return this
   }
 }
 
 
-
-let list = new SinglyLinkedList;
+let list = new SinglyLinkedList()
 list.push("Hello")
 list.push("Goodbye")
 list.push("!")
@@ -113,5 +183,21 @@ console.log(list)
 list.shift()
 console.log(list)
 
-list.unshift(10);
+list.unshift(10)
+console.log(list)
+
+console.log(list.get(2))
+console.log(list.get(0))
+
+console.log(list.set(0, "test"))
+console.log(list.get(0))
+
+
+list.insert(1,'beans')
+
+console.log(list) 
+list.remove(2)
+console.log(list)
+
+list.reverse()
 console.log(list)
