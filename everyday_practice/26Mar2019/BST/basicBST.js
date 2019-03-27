ac67944128922657d45e459199ebb78b0c828ec8
brandon-schabel <brandonschabel1995@gmail.com>
2019-03-27 08:31:23 -0700

    basic binary search tree

---
+++ everyday_practice/26Mar2019/BST/basicBST.js
@@ -0,0 +1,20 @@
+class BinarySearchTree {
+  constructor() {
+    this.root = null
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value
+    this.left = null
+    this.right = null
+  }
+}
+
+let tree = new BinarySearchTree();
+tree.root = new Node(10)
+tree.root.right = new Node(15)
+tree.root.left = new Node(7)
+
+console.log(tree)
