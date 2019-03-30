6f86495e11c1b3d30584e01e4c3757882b498e82
brandon-schabel <brandonschabel1995@gmail.com>
2019-03-30 10:08:23 -0700

    reverse string no built in methods

--- everyday_practice/30Mar2019/reverseStringNoMethods.js
+++ everyday_practice/30Mar2019/reverseStringNoMethods.js
@@ -7,4 +7,10 @@ const reverseString = (string) => {
   return newString;
 }
 
+console.time('reverse string no method')
 console.log(reverseString('test'))
+console.timeEnd('reverse string no method')
+
+console.time('reverse w/ built in methods')
+'test'.split('').reverse().join('')
+console.timeEnd('reverse w/ built in methods')
