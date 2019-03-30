a6dc92a54d602e20efe8711b409955eb7b2c9b07
brandon-schabel <brandonschabel1995@gmail.com>
2019-03-30 10:02:15 -0700

    reverse string with no built in string methods

---
+++ everyday_practice/30Mar2019/reverseStringNoMethods.js
@@ -0,0 +1,10 @@
+const reverseString = (string) => {
+  let newString = '';
+  for(let i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+
+  return newString;
+}
+
+console.log(reverseString('test'))
